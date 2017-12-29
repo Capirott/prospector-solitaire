8fede913f569c9904a7590bceae061adc58c1b44
Erickson Silva <erickson.lucas@live.com>
2017-12-29 00:17:11 -0200

    Game in progress x)

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +8,14 @@ public class Card : MonoBehaviour {
     [Header("Set Dynamically")]
     public string suit;
     public int rank;
-    public Color color = Color.black; 
-    public string colS = "Black"; 
+    public Color color = Color.black;
+    public string colS = "Black";
     public List<GameObject> decoGOs = new List<GameObject>();
     public List<GameObject> pipGOs = new List<GameObject>();
-    public GameObject back; 
-    public CardDefinition def; 
-    
+    public GameObject back;
+    public CardDefinition def;
+    public SpriteRenderer[] spriteRenderers;
+
     public bool faceUp
     {
         get
@@ -26,14 +28,58 @@ public class Card : MonoBehaviour {
         }
     }
 
+    public void PopulateSpriteRenderes()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderes();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
     void Start () {
+        SetSortOrder(0);
+	}
+
+    public void SetSortOrder(int v)
+    {
+        PopulateSpriteRenderes();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = v;
+                continue;
+            }
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = v + 2;
+                    break;
+                case "face":
+                default:
+                    tSR.sortingOrder = v + 1;
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
 }
 
 
