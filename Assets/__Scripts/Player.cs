7b724b2425e0bbc94536f18910b91d88d4fece1f
Erickson Silva <erickson.lucas@live.com>
2017-12-30 01:31:38 -0200

    Game in progress.

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum PlayerType
+{
+    human,
+    ai
+}
+
+[System.Serializable]
+public class Player
+{
+
+    public PlayerType type = PlayerType.ai;
+    public int playerNum;
+    public SlotDef handSlotDef;
+    public List<CardBartok> hand;
+
+
+    public CardBartok AddCard(CardBartok eCB)
+    {
+        if (hand == null) hand = new List<CardBartok>();
+        hand.Add(eCB);
+        return (eCB);
+    }
+
+    public CardBartok RemoveCard(CardBartok cb)
+    {
+        if (hand == null || !hand.Contains(cb)) return null;
+        hand.Remove(cb);
+        return (cb);
+    }
+
+        void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
